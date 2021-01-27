ed1636acc10dbdc0e91d9f73ace80e999e6024ad
苏柚 <467162434@qq.com>
2021-01-27 22:34:08 +0800

    格力机器人程序 添加了休眠时间 减少cpu占用

--- GreeBot.py
+++ GreeBot.py
@@ -2,7 +2,6 @@
 格力机器人
 """
 from GreeMessage import GreeMessage
-import time
 from BaiduTran import translation
 import time
 
@@ -23,18 +22,16 @@ class GreeBot(object):
         start_time = time.time()
         _, last_msgId = self.message.get_message()
         while True:
-            if time.time() - start_time > 2:
-                content, msgId = self.message.get_message()
-                if msgId != last_msgId:
-                    print(content)
-                    last_msgId = msgId
-                    if ':tr' in content:
-                        dst = translation(content[3:])
-                        print(dst)
-                        self.message.send_message(dst)
+            content, msgId = self.message.get_message()
+            if msgId != last_msgId:
+                print(content)
+                last_msgId = msgId
+                if ':tr' in content:
+                    dst = translation(content[3:])
+                    print(dst)
+                    self.message.send_message(dst)
 
-                start_time = time.time()
-            # time.sleep(1)
+            time.sleep(1)
 
 
 bot = GreeBot()
